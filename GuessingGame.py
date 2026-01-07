dc2ae88d1fdd47dc9efdf5a83501887e05e8c8b7
username <useremail>
2026-01-06 22:12:58 -0800

    Add project files.

---
+++ GuessingGame.py
@@ -0,0 +1,15 @@
+#Dominick Orlando 
+#CIS 261 
+#Guessing Game Introduction
+
+import math 
+
+print("Guess the number!")
+
+limit = int(input("Enter the limit: "))
+
+print("I'm thinking of a number from one to" limit) 
+
+guess = input("Your guess: ")
+
+if guess
