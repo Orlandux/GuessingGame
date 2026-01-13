0c7440dd80864c22a6d2509c1b8995d632ac4c8a
username <useremail>
2026-01-13 13:19:55 -0800

    Guessing Game

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,15 +1,33 @@
 #Dominick Orlando 
 #CIS 261 
 #Guessing Game Introduction
+ 
+import random 
 
-import math 
 
 print("Guess the number!")
 
-limit = int(input("Enter the limit: "))
+play_again = "y"
 
-print("I'm thinking of a number from one to" limit) 
+while play_again == "y":
 
-guess = input("Your guess: ")
+    limit = int(input("Enter the limit: "))
+    print("I'm thinking of a number from 1 to", limit) 
+    tries = 0 
+    number = random.randint(1, limit) 
 
-if guess
+    while True:
+        guess = int(input("Your guess: ")) 
+        tries += 1
+
+        if guess == number: 
+            print("Your guessed it right in", tries, "tries!")
+            break 
+        elif guess > number:
+            print("Too high. Try again.")
+        else:
+            print("Too low. Try again.")
+
+    play_again = input("Play again? (y/n): ") 
+
+print("Thanks for playing!")
